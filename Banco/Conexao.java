0e0cb879930969fe150aff067e479c233e8787a3
NatanaelQueiroz <natanqueiroz77@gmail.com>
2022-08-12 17:13:18 -0300

    criação da conexão do banco de dados

---
+++ Banco/Conexao.java
@@ -0,0 +1,41 @@
+package Banco;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	private String user;
+	private String url;
+	private String psw;
+	private String driver;
+	
+	Connection con = null;
+	
+	public Connection conectar() {
+		url = "jdbc:mysql://localhost:3306/banco?characterEncoding=utf8&useSSL=false&useUnicode=true";
+		user = "root";
+		psw = "mysql";
+		driver = "com.mysql.jdbc.Driver";
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, psw);
+			System.out.println("Conectado com sucesso! ");
+			
+		} catch (ClassNotFoundException Driver) {
+			System.out.println("Erro não foi possível achar o driver de conexão."+Driver);
+			
+		} catch (SQLException erro) {
+			System.out.println("Erro não foi possível se conectar com o DB."+erro);
+		}
+		
+		return con;
+	}
+	
+	public static void main (String [] args) {
+		
+		Conexao co = new Conexao();
+		co.conectar();
+	}
+}
